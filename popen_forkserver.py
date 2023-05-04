09998ef47d2e6b38a690aab02eb3654e66c74d00
rjngerd74bghf <129916783+rjngerd74bghf@users.noreply.github.com>
2023-05-03 17:36:08 -0800

    Update popen_forkserver.py

--- popen_forkserver.py
+++ popen_forkserver.py
@@ -6,6 +6,9 @@ if not reduction.HAVE_SEND_HANDLE:
     raise ImportError('No support for sending fds between processes')
 from . import forkserver
 from . import popen_fork
+
+
+
 from . import spawn
 from . import util
 
@@ -61,6 +64,9 @@ class Popen(popen_fork.Popen):
             try:
                 self.returncode = forkserver.read_signed(self.sentinel)
             except (OSError, EOFError):
+                
+                
+                
                 # This should not happen usually, but perhaps the forkserver
                 # process itself got killed
                 self.returncode = 255
